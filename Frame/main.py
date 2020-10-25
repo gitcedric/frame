34045cd3a66348d88ea986d6bf304b05ec9235d7
gitcedric <62556951+gitcedric@users.noreply.github.com>
2020-10-25 17:30:37 +0000

    Add files via upload

---
+++ Frame/main.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+'''
+@Project Pic Frame
+@author cedric
+@date 2020-08-16
+'''
+
+#tkinter imports
+from tkinter import *
+from PIL import ImageTk, Image 
+
+#files
+import json
+from os import walk
+from os.path import dirname, isfile, abspath, getmtime, join
+
+#logic
+import time
+import _thread
+
+#parse config
+config = json.load(open("Config.json"))
+path_to_img = config['files']['path']
+sleeptime = config['display']['displaytime_in_seconds']
+
+#variables
+path_to_dir = abspath(dirname(__file__))
+dir_to_img = '/'+path_to_img
+files = []
+
+#Caching all files found in subdir 'img/'
+def cache_files():
+    global files
+    files = []
+    for (dirpath, dirnames, filenames) in walk(path_to_dir+dir_to_img):
+        for fname in sorted(filenames, key=lambda name:
+                    getmtime(join(dirpath, name))):
+            files.append(fname)
+    print(str(len(files))+' files cached!')
+
+#running method before init of app
+cache_files()
+
+class Window(Frame):
+
+    def __init__(self, parent=None):
+        Frame.__init__(self, parent)
+        self.parent = parent
+        self.pack(fill=BOTH, expand=YES)
+
+        self.img = Label(self, image="")
+        self.img.config(background='black')
+        self.img.grid(row=0, column=0) 
+        self.img.place(relx = 0.5, rely = 0.5, anchor = 'center')
+        #start thread for walking through files
+        _thread.start_new_thread(self.nextfile, ())
+    
+    #change image path and reload it from disk
+    def changeImage(self, filename):
+        render = self.openImage(filename)
+        self.img.configure(image=render)
+        self.img.image=render
+    
+    #iterate over files to call next image
+    def nextfile(self):
+        while (True):
+            for fname in files:
+                if isfile(abspath(path_to_dir)+dir_to_img+fname):
+                    self.changeImage(fname)
+                    time.sleep(sleeptime)
+            #cache new files
+            cache_files()
+        #runs in infinite loop!
+
+    #open Image, convert it to fit either landscape or portrait and return as PhotoImage
+    def openImage(self, filename):
+        image = Image.open(path_to_img+filename)
+        screensize = root.winfo_screenwidth(), root.winfo_screenheight()
+
+        #fit longer axis to screen, shorter axis = image * (screen/image)
+        #size = [width, height]
+        if image.size[0]>image.size[1]:
+            newSize = screensize[0], int(image.size[1]*(screensize[0]/image.size[0]))
+        else:
+            newSize = int(image.size[0]*(screensize[1]/image.size[1])), screensize[1]
+
+        return ImageTk.PhotoImage(image.resize(newSize, Image.ANTIALIAS))
+
+
+root = Tk()
+app = Window(root)
+root.title("Frame")
+# root.geometry("480x480") #enable for windowed
+root.attributes('-fullscreen', True) #enable for fullscreen
+app.config(background='black', cursor='none')
+root.mainloop() 
---
+++ Frame/readmail.py
@@ -0,0 +1,73 @@
+import imaplib
+import email
+import json
+import base64
+import os
+from os.path import dirname, abspath
+
+#parse config
+config = json.load(open("Config.json"))
+user = config['mail']['login']
+settings = config['mail']['settings']
+fileconfig = config['files']
+
+#img filepath
+path_to_dir = abspath(dirname(__file__))
+filepath = path_to_dir+'/'+fileconfig['path']
+max_foldersize = fileconfig['max_foldersize']
+mail_folder = settings['folder']
+
+
+#create IMAP with SSL
+imap = imaplib.IMAP4_SSL(settings['imap'], settings['port'])
+
+#auth
+imap.login(user['mail'], user['password'])
+
+status, messages = imap.select(mail_folder)
+
+type, data = imap.search(None, 'UNSEEN')
+mail_ids = data [0]
+id_list = mail_ids.split()
+
+for num in data[0].split():
+    typ, data = imap.fetch(num, '(RFC822)' )
+    raw_mail = data[0][1]
+    
+    #converts byte literal to string
+    email_message = email.message_from_string(raw_mail.decode('utf-8'))
+    
+    #download attachements
+    for part in email_message.walk():
+        #..
+        if part.get_content_maintype() == 'multipart':
+            continue
+        if part.get('Content-Disposition') is None:
+            continue
+        fileName = part.get_filename()
+            
+        if bool(fileName):
+            filePath = os.path.join(filepath, fileName)
+            if not os.path.isfile(filePath) and not fileName.endswith(".txt"):
+                fp = open(filePath, 'wb')
+                fp.write(part.get_payload(decode=True))
+                fp.close
+            
+            subject = str(email_message).split("Subject: ", 1)[1].split("\nTo:", 1)[0]
+            print('Downloaded "{fileName}" from email to"{path}".'.format(fileName=fileName, path=filepath))
+            
+            #if more than X files, delete oldest one
+            list_of_files=os.listdir(filepath)
+            full_path = [filepath+"{0}".format(x) for x in list_of_files]
+            
+            if len(list_of_files) > max_foldersize:
+                os.remove(min(full_path, key=os.path.getctime))
+                print('Exceeded max_foldersize of ' + str(max_foldersize) + ', deleting oldest file.')
+        
+    
+    imap.store(num, '+FLAGS', '\Deleted')
+
+imap.close()
+imap.logout()
+
+print('Done...')
